91d1f568e83b2bd8a05f91ea1e2c0927d1f6dd2e
Sofija Dišlovaja <sofija.dyshlovaya@gmail.com>
2023-03-29 09:19:27 +0300

    Izveidots pirmais skripts

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+	private string teksts;
+	private string[] varianti = {"Sveiks ", "Jauku dienu ", "Prieks tevi redzét ", "Uzredzéśanos "};
+
+	int nejausais;
+
+	public GameObject ievadesLauks;
+	public GameObject tesktaAttelosana;
+
+	public void UzglabatTekstu()
+	{
+		nejausais = Random.Range(0, varianti.Length); //0 - smallest possible, varianti.length - largest possible
+		teksts = ievadesLauks.GetComponent<Text>().text;
+        tesktaAttelosana.GetComponent<Text> ().text = varianti[nejausais]+teksts.ToUpper()+"!";
+
+    }
+}
